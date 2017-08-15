b9cb792079fc177e766aec05dceaae8e6758c6c6
timm0e <11885527+timm0e@users.noreply.github.com>
2017-08-16 01:48:37 +0200

    init

---
+++ public/javascripts/sounds.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $(".category").click(function () {
+        $(".category").removeClass("active");
+        $(this).addClass("active");
+    });
+});
---
+++ public/javascripts/sounds.ts
@@ -0,0 +1,6 @@
+$(document).ready(()=>{
+    $(".category").click(function(){
+        $(".category").removeClass("active");
+        $(this).addClass("active");
+    })
+})
